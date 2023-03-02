6eec7c4552774cfe505c1c73f6c6ff5e14dec60c
Jake Wehder <jw@email.comjake>
2023-03-01 20:47:00 -0600

    commit tree db8791cf6c83077018e210cba52c2fe480d68bef

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,33 @@
 # your code here
+class Stack
+
+    def initialize(limit)
+        @stack = []
+        @limit = limit
+    end
+
+    def push(value)
+        @stack.length == @limit ? raise "Stack Overflow" : @stack.push(value)
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack.last
+    end
+
+    def size
+        @stack.length
+    end
+
+    def empty?
+        @stack.empty?
+    end
+
+    def full
+        @stack.size == @limit 
+    end
+
+end
